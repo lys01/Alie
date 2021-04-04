23adac92600cec063bf98712f77a0048a6b95da4
tubie75 <80998734+tubie75@users.noreply.github.com>
2021-04-04 10:16:39 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -29,7 +29,7 @@ let cookiesArr = [], cookie = '', jdPetShareArr = [], isBox = false, notify, new
 //助力好友分享码(最多5个,否则后面的助力失败),原因:京东农场每人每天只有四次助力机会
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
-let shareCodes = ['MTEzMzI0OTE0NTAwMDAwMDA0MjgwOTEwNw==@MTE1NDUwMTI0MDAwMDAwMDQ2NTg3MDkz@MTAxODc2NTEzMTAwMDAwMDAyMzQ4MTk4MQ==@MTE1NDUwMTI0MDAwMDAwMDQ0NzI1NzQ1',''
+let shareCodes = [''//'MTEzMzI0OTE0NTAwMDAwMDA0MjgwOTEwNw==@MTE1NDUwMTI0MDAwMDAwMDQ2NTg3MDkz@MTAxODc2NTEzMTAwMDAwMDAyMzQ4MTk4MQ==@MTE1NDUwMTI0MDAwMDAwMDQ0NzI1NzQ1',''
 ]
 let message = '', subTitle = '', option = {};
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
