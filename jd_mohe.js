83ccd5b2ff81be01e8ab22880d73c020ae754c7e
tubie75 <80998734+tubie75@users.noreply.github.com>
2021-04-06 19:35:55 +0800

    Add files via upload

--- jd_mohe.js
+++ jd_mohe.js
@@ -37,7 +37,7 @@ if ($.isNode()) {
 
 const JD_API_HOST = 'https://isp5g.m.jd.com';
 //邀请码一天一变化，已确定
-$.shareId = ['8d3a3f50-8e8e-446c-8696-3a08c538c716','2fc84788-4f9c-40d6-a6fc-2d2fe9d8ea3c'];
+$.shareId = ['995f7b7c-249a-4022-a51e-250393649764','995f7b7c-249a-4022-a51e-250393649764'];
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
