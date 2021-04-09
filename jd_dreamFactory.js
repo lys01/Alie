c9c777127d487b6c94bec09854c798390604ddd1
tubie75 <80998734+tubie75@users.noreply.github.com>
2021-04-09 23:03:46 +0800

    Update jd_dreamFactory.js

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -42,8 +42,8 @@ const randomCount = $.isNode() ? 20 : 5;
 let tuanActiveId = `0_pzMedR7KhclCkMIgkTkg==`;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
-const inviteCodes = ['p15jzNJ363IRRYrz5uiijQ=='
-];
+const inviteCodes = ['p15jzNJ363IRRYrz5uiijQ==@kMVrN_EU0rOG1TiQJoSHjg==',
+                     'p15jzNJ363IRRYrz5uiijQ==@kMVrN_EU0rOG1TiQJoSHjg=='];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 $.tuanIds = [];
 $.appId = 10001;
