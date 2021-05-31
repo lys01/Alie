c1109d690278251406839be875311258a9c600c6
tubie75 <80998734+tubie75@users.noreply.github.com>
2021-05-31 11:04:06 +0800

    Add files via upload

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -965,8 +965,8 @@ async function tuanActivity() {
   }
 }
 async function joinLeaderTuan() {
-  let res = await updateTuanIdsCDN(), res2 = await updateTuanIdsCDN("http://cdn.annnibb.me/factory.json")
-  if (!res) res = await updateTuanIdsCDN('https://cdn.jsdelivr.net/gh/gitupdate/updateTeam@master/shareCodes/jd_updateFactoryTuanId.json');
+  let res = await updateTuanIdsCDN(), res2 = await updateTuanIdsCDN("https://ghproxy.com/https://raw.githubusercontent.com/dantes1016/gaqd/main/shareCodes/jd_updateFactoryTuanId.json")
+  if (!res) res = await updateTuanIdsCDN('https://raw.githubusercontent.com/dantes1016/gaqd/main/shareCodes/jd_updateFactoryTuanId.json');
   $.authorTuanIds = [...(res && res.tuanIds || []),...(res2 && res2.tuanIds || [])]
   if ($.authorTuanIds && $.authorTuanIds.length) {
     for (let tuanId of $.authorTuanIds) {
