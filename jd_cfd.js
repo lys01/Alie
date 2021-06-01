0411e7343ff5d3dfd0171770d9e864c3c988e66c
tubie75 <80998734+tubie75@users.noreply.github.com>
2021-06-01 09:59:34 +0800

    Update jd_cfd.js

--- jd_cfd.js
+++ jd_cfd.js
@@ -28,8 +28,8 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
 $.showLog = $.getdata("cfd_showLog") ? $.getdata("cfd_showLog") === "true" : false;
 $.notifyTime = $.getdata("cfd_notifyTime");
-$.result = [];$.shareCodes = ['Jxcfd_GroupId_310_62449693',
-                              'Jxcfd_GroupId_310_62443958',
+$.result = [];$.shareCodes = ['Jxcfd_GroupId_313_63060894',
+                              'Jxcfd_GroupId_313_63069788',
 ];
 let cookiesArr = [], cookie = '', token;
 
